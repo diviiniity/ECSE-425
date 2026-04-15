255f08766003593e082faf7214c901ed31cabbd3
Jake <46657160+jacobkogut@users.noreply.github.com>
2026-04-14 21:14:33 -0400

    fix tcl

--- P4/testbench.tcl
+++ P4/testbench.tcl
@@ -3,6 +3,7 @@ vlib work
 vmap work work
 
 vcom -2008 -work work memory.vhd
+vcom -2008 -work work package.vhd
 vcom -2008 -work work register_file.vhd
 vcom -2008 -work work cpu_subfiles/branch_logic.vhd
 vcom -2008 -work work cpu_subfiles/ALU_decoder.vhd
