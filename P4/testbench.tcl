72fd407840ae5cf690c1c62fd6e10431ef147cb2
Nizar <nizar.kheireddine@mail.mcgill.ca>
2026-04-14 22:55:57 -0400

    changed tcl to be run from outside p4

--- P4/testbench.tcl
+++ P4/testbench.tcl
@@ -2,17 +2,17 @@
 vlib work
 vmap work work
 
-vcom -2008 -work work memory.vhd
-vcom -2008 -work work package.vhd
-vcom -2008 -work work register_file.vhd
-vcom -2008 -work work cpu_subfiles/branch_logic.vhd
-vcom -2008 -work work cpu_subfiles/ALU_decoder.vhd
-vcom -2008 -work work cpu_subfiles/Main_decoder.vhd
-vcom -2008 -work work cpu_subfiles/Control_unit.vhd
-vcom -2008 -work work cpu_subfiles/ALU.vhd
-vcom -2008 -work work cpu_subfiles/Imm_extension.vhd
-vcom -2008 -work work cpu.vhd
-vcom -2008 -work work cpu_tb.vhd
+vcom -2008 -work work p4/memory.vhd
+vcom -2008 -work work p4/package.vhd
+vcom -2008 -work work p4/register_file.vhd
+vcom -2008 -work work p4/cpu_subfiles/branch_logic.vhd
+vcom -2008 -work work p4/cpu_subfiles/ALU_decoder.vhd
+vcom -2008 -work work p4/cpu_subfiles/Main_decoder.vhd
+vcom -2008 -work work p4/cpu_subfiles/Control_unit.vhd
+vcom -2008 -work work p4/cpu_subfiles/ALU.vhd
+vcom -2008 -work work p4/cpu_subfiles/Imm_extension.vhd
+vcom -2008 -work work p4/cpu.vhd
+vcom -2008 -work work p4/cpu_tb.vhd
 
 
 vsim -t 1ps work.cpu_tb
